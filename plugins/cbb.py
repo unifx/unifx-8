d6e6b5f085486035a79180ebf36c64e29eac2e7b
unifx <72940273+unifx@users.noreply.github.com>
2023-03-24 20:03:47 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -17,10 +17,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Informasi.\n\n • OWNER REPO : @satyaalfarabiii\n • CHANNEL : <a href='https://t.me/storesatya'>JOIN</a>\n • SOURCE CODE : <a href='https://t.me/satya_alfarabi'>JOIN</a>\n\n Support @bgsatbot</b>\n",
+            text=f"<b>Informasi.\n\n • OWNER REPO : <a href='https://t.me/satyaalfarabiii'>𝐒𝐚𝐭𝐲𝐚 𝐀𝐥𝐟𝐚𝐫𝐚𝐛𝐢</a>\n • CHANNEL : <a href='https://t.me/storesatya'>𝐉𝐎𝐈𝐍</a>\n • SOURCE CODE : <a href='https://t.me/satya_alfarabi'>𝐤𝐥𝐢𝐤 𝐝𝐢𝐬𝐢𝐧𝐢</a>\n\n Support @bgsatbot</b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• 𝗧𝗨𝗧𝗨𝗣 •", callback_data="close")]]
+                [[InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]]
             ),
         )
     elif data == "close":
